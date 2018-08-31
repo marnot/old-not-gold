8e2ad12070dac3e92152c974ab838b0ce31730c4
Marcin Banot <emer.been@gmail.com>
2018-08-31 12:18:41 +0200

    Update connection.php

--- coderslab/Tillter-master/connection.php
+++ coderslab/Tillter-master/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = 'localhost';
-$username = 'root';
-$password = 'coderslab';
+$host = '';
+$username = '';
+$password = '';
 $db = 'tillter';
 
 $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $password);
